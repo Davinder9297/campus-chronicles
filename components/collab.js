ea3e93182976aebc46b8d561a44682b5bbcfe25d
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2023-02-27 12:36:36 +0530

    Jas Commit

--- components/collab.js
+++ components/collab.js
@@ -4,7 +4,7 @@
 <div className="text-center text-3xl font-semibold text-gray-600 font-serif">Our Recruiters</div>
 <div className="flex justify-center"><img src="hr.png" alt="" /></div>
 <div className="flex-col  h-auto mt-5 w-full ">
-  <div className="flex animate-move">
+  <div className="flex animate-move justify-around ">
   <img className="h-28   " src="a.png" alt="" />
     <img className="h-28  " src="b.png" alt="" />
     <img className="h-28  " src="c.png" alt="" />
@@ -12,7 +12,7 @@
     <img className="h-28  " src="e.png" alt="" />
     <img className="h-28  " src="f.png" alt="" />
   </div>
-   <div className="flex animate-move2">
+   <div className="flex animate-move2 justify-around">
    <img className="h-28  " src="g.png" alt="" />
     <img className="h-28  " src="i.png" alt="" />
     <img className="h-28  " src="k.png" alt="" />
---
+++ pages/cultural_club.js
@@ -0,0 +1,130 @@
+export default function Culturalclub() {
+    return (<>
+
+        <div className="border-2 border-solid border-pink-600 w-full ">
+            <div className=" w-full h-44 overflow-hidden shadow-2xl shadow-slate-500">
+                <img className="h-full" src="cultural_tile.png" />
+            </div>
+            <div className="flex flex-row border-2 border-solid border-yellow-500" >
+                <div className="border-2 border-solid border-blue-600 w-[40%] text-center font-bold text-3xl font-sans m-3 p-2">
+                    Our Handlers
+                    <div className="border-2 border-solid border-green-600 text-2xl m-3 p-3 rounded-xl">
+                        Incharge
+                        <div className="flex flex-row border-2 justify-around border-solid border-green-600 ">
+                            <div className="border-2 border-solid border-yellow-600 w-[40%] text-xl h-32">
+                                img
+                            </div>
+                            <div className="border-2 border-solid border-red-600 w-[95%] text-xl h-32">
+                                Name
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="border-2 border-solid border-green-600 text-2xl m-3 p-3 rounded-xl">
+                        President
+                        <div className="flex flex-row border-2 justify-around border-solid border-green-600 ">
+                            <div className="border-2 border-solid border-yellow-600 w-[40%] text-xl h-32">
+                                img
+                            </div>
+                            <div className="border-2 border-solid border-red-600 w-[95%] text-xl h-32">
+                                Name
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="border-2 border-solid border-green-600 text-2xl m-3 p-3 rounded-xl">
+                        Leading Roles<br />
+                        <div className="flex flex-row border-2 justify-around border-solid border-green-600 ">
+                            <div className="border-2 border-solid border-yellow-600 w-[40%] text-xl h-32 ">
+                                img
+                            </div>
+                            <div className="border-2 border-solid border-red-600 w-[95%] text-xl h-32">
+                                Name
+                            </div>
+                        </div>
+                        <div className="flex flex-row border-2 justify-around border-solid border-green-600 ">
+                            <div className="border-2 border-solid border-yellow-600 w-[40%] text-xl h-32 ">
+                                img
+                            </div>
+                            <div className="border-2 border-solid border-red-600 w-[95%] text-xl h-32">
+                                Name
+                            </div>
+                        </div>
+                        <div className="flex flex-row border-2 justify-around border-solid border-green-600 ">
+                            <div className="border-2 border-solid border-yellow-600 w-[40%] text-xl h-32 ">
+                                img
+                            </div>
+                            <div className="border-2 border-solid border-red-600 w-[95%] text-xl h-32">
+                                Name
+                            </div>
+                        </div>
+                        <div className="flex flex-row border-2 justify-around border-solid border-green-600 ">
+                            <div className="border-2 border-solid border-yellow-600 w-[40%] text-xl h-32 ">
+                                img
+                            </div>
+                            <div className="border-2 border-solid border-red-600 w-[95%] text-xl h-32">
+                                Name
+                            </div>
+                        </div>
+                        <div className="flex flex-row border-2 justify-around border-solid border-green-600 ">
+                            <div className="border-2 border-solid border-yellow-600 w-[40%] text-xl h-32 ">
+                                img
+                            </div>
+                            <div className="border-2 border-solid border-red-600 w-[95%] text-xl h-32">
+                                Name
+                            </div>
+                        </div>
+                        <div className="flex flex-row border-2 justify-around border-solid border-green-600 ">
+                            <div className="border-2 border-solid border-yellow-600 w-[40%] text-xl h-32 ">
+                                img
+                            </div>
+                            <div className="border-2 border-solid border-red-600 w-[95%] text-xl h-32">
+                                Name
+                            </div>
+                        </div>
+                        <div className="flex flex-row border-2 justify-around border-solid border-green-600 ">
+                            <div className="border-2 border-solid border-yellow-600 w-[40%] text-xl h-32 ">
+                                img
+                            </div>
+                            <div className="border-2 border-solid border-red-600 w-[95%] text-xl h-32">
+                                Name
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="border-2 border-solid border-blue-600 w-[60%] text-center font-bold text-3xl font-sans m-3 p-2">
+                    Our Events....
+                    <div className="flex flex-wrap justify-center border-solid border-2 m-3 border-orange-600">
+
+                        <div className="border-solid border-2 m-3 border-pink-600 h-[250px] rounded-xl shadow-2xl shadow-slate-900 cursor-pointer hover:scale-105 transition duration-150 w-[45%]">
+                            <img className="rounded-xl h-full w-full" src="cultural_logo.png" />
+
+                        </div>
+                        <div className="border-solid border-2 m-3 border-pink-600 h-[250px] rounded-xl shadow-2xl shadow-slate-900 cursor-pointer hover:scale-105 transition duration-150 w-[45%]">
+                            <img className="rounded-xl h-full w-full" src="personality.png" />
+                        </div>
+                        <div className="border-solid border-2 m-3 border-pink-600 h-[250px] rounded-xl shadow-2xl shadow-slate-900 cursor-pointer hover:scale-105 transition duration-150 w-[45%]">
+                            <img className="rounded-xl h-full w-full" src="editorial.jpg" />
+                        </div>
+                        <div className="border-solid border-2 m-3 border-pink-600 h-[250px] rounded-xl shadow-2xl shadow-slate-900 cursor-pointer hover:scale-105 transition duration-150 w-[45%]">
+                            <img className="rounded-xl h-full w-full" src="cultural_logo.png" />
+                        </div>
+                        <div className="border-solid border-2 m-3 border-pink-600 h-[250px] rounded-xl shadow-2xl shadow-slate-900 cursor-pointer hover:scale-105 transition duration-150 w-[45%]">
+                            <img className="rounded-xl h-full w-full" src="personality.png" />
+                        </div>
+                        <div className="border-solid border-2 m-3 border-pink-600 h-[250px] rounded-xl shadow-2xl shadow-slate-900 cursor-pointer hover:scale-105 transition duration-150 w-[45%]">
+                            <img className="rounded-xl h-full w-full" src="editorial.jpg" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossOrigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
+    </>)
+}
+
+
