9991989b5205df29de6303aec4655ea9d8d34968
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2023-02-28 11:50:42 +0530

    Jas 2  Commit

--- pages/personality_club.js
+++ pages/personality_club.js
@@ -2,42 +2,42 @@ export default function Culturalclub() {
     return (<>
 
         <div className=" w-full mt-1">
-            <div className=" w-full h-44 overflow-hidden shadow-xl shadow-red-200 ">
-                <img className="h-full" src="cultural_tile.png" />
+            <div className=" w-full h-44 overflow-hidden shadow-xl shadow-orange-300 ">
+                <img className="h-full w-full" src="personality_tile.png" />
             </div>
-            <div className="flex flex-row " >
+            <div className="flex flex-row m-3 mt-4 " >
                 <div className="w-[40%] text-center font-bold text-3xl font-sans m-3 p-2">
                     Our Handlers
-                    <div className="border-2 border-solid border-red-600 text-2xl m-3 p-3 rounded-xl">
-                    <div className="bg-red-200 rounded-xl">Incharge</div>
+                    <div className="border-2 border-solid border-orange-600 text-2xl m-3 p-3 rounded-xl">
+                    <div className="bg-orange-200 rounded-xl">Incharge</div>
                         <div className="flex flex-row  justify-around  ">
                             <div className="w-[40%] text-xl h-28">
                                 <img className="h-full w-full" src="teacher.jpg" />
                             </div>
                             <div className="w-[95%] text-xl h-full my-auto items-center m-auto">
-                                <div >Mr. Malkiat Singh</div>
-                                <div className="font-normal text-sm">(Senior Assistant)</div>
+                                <div >Dr. Geet</div>
+                                <div className="font-normal text-sm">(Deputy Registrar)</div>
                             </div>
                         </div>
                     </div>
 
-                    <div className="border-2 border-solid border-red-600 text-2xl m-3 p-3 rounded-xl">
-                    <div className="bg-red-200 rounded-xl">President</div>
+                    <div className="border-2 border-solid border-orange-600 text-2xl m-3 p-3 rounded-xl ">
+                        <div className="bg-orange-200 rounded-xl">President</div>
                         <div className="flex flex-row  justify-around  ">
                             <div className="w-[40%] text-xl h-28">
                                 <img className="h-full w-full" src="teacher.jpg" />
                             </div>
                             <div className="w-[95%] text-xl h-full my-auto items-center m-auto">
-                                <div className="">Ajay Kumar</div>
-                                <div className="font-normal text-sm">(3<sup>rd</sup> Year )</div>
+                                <div className="">Kashvi Sinha</div>
+                                <div className="font-normal text-sm">(2<sup>nd</sup> Year )</div>
 
                             </div>
                         </div>
                     </div>
 
-                    <div className="border-2 border-solid border-red-600 text-2xl m-3 p-3 rounded-xl">
-                    <div className="bg-red-200 rounded-xl">Leading Roles</div>
-                        <div className="flex flex-row justify-around  mt-3 border-2 border-solid border-red-600 bg-red-400 rounded-xl">
+                    <div className="border-2 border-solid border-orange-600 text-2xl m-3 p-3 rounded-xl">
+                        Leading Roles
+                        <div className="flex flex-row justify-around  mt-3 border-2 border-solid border-orange-600 bg-orange-400">
                             <div className="w-[20%] text-xl h-[58] p-1">
                                 Sno.
                             </div>
@@ -53,7 +53,7 @@ export default function Culturalclub() {
                                 1.
                             </div>
                             <div className=" w-[55%] text-xl h-[58] text-left">
-                                Jashanjot Kaur
+                                Ayush Sharma
                             </div>
                             <div className=" w-[20%] text-xl h-[58] text-left">
                                 3<sup>rd</sup> Year
@@ -64,7 +64,7 @@ export default function Culturalclub() {
                                 2.
                             </div>
                             <div className=" w-[55%] text-xl h-[58] text-left">
-                                Tania
+                                Umar
                             </div>
                             <div className=" w-[20%] text-xl h-[58] text-left">
                                 2<sup>nd</sup> Year
@@ -75,7 +75,7 @@ export default function Culturalclub() {
                                 3.
                             </div>
                             <div className=" w-[55%] text-xl h-[58] text-left">
-                                Sahil
+                                Yash
                             </div>
                             <div className=" w-[20%] text-xl h-[58] text-left">
                                 2<sup>nd</sup> Year
@@ -86,7 +86,7 @@ export default function Culturalclub() {
                                 4.
                             </div>
                             <div className=" w-[55%] text-xl h-[58] text-left">
-                                Anamika
+                                Vansh
                             </div>
                             <div className=" w-[20%] text-xl h-[58] text-left">
                                 2<sup>nd</sup> Year
@@ -97,7 +97,7 @@ export default function Culturalclub() {
                                 5.
                             </div>
                             <div className=" w-[55%] text-xl h-[58] text-left">
-                                Vansh
+                                Harivansh
                             </div>
                             <div className=" w-[20%] text-xl h-[58] text-left">
                                 2<sup>nd</sup> Year
@@ -108,10 +108,10 @@ export default function Culturalclub() {
                                 6.
                             </div>
                             <div className=" w-[55%] text-xl h-[58] text-left">
-                                Garima
+                                Utkarsh
                             </div>
                             <div className=" w-[20%] text-xl h-[58] text-left">
-                                2<sup>nd</sup> Year
+                                1<sup>st</sup> Year
                             </div>
                         </div>
                         <div className="flex flex-row justify-around  ">
@@ -119,66 +119,56 @@ export default function Culturalclub() {
                                 7.
                             </div>
                             <div className=" w-[55%] text-xl h-[58] text-left">
-                                Simran Saka
+                                Nikhilpreet Saini
                             </div>
                             <div className=" w-[20%] text-xl h-[58] text-left">
                                 1<sup>st</sup> Year
                             </div>
                         </div>
-                        <div className="flex flex-row justify-around  ">
-                            <div className="w-[20%] text-xl h-[58] p-1">
-                                8.
-                            </div>
-                            <div className=" w-[55%] text-xl h-[58] text-left">
-                                Ananya Bisht
-                            </div>
-                            <div className=" w-[20%] text-xl h-[58] text-left">
-                                1<sup>st</sup> Year
-                            </div>
-                        </div>
+                        
                     </div>
                 </div>
+
                 <div className="flex-col m-3 w-[68%]">
                     <div className="  text-center font-bold text-3xl font-sans p-2">
                         About us....
-                        <div className="m-3 font-normal text-xl border-solid border-2 border-red-600 rounded-xl p-2 text-justify">
-                        Culture club helps in encouraging talent of the students. Club activities help students to develop a sense of unity and teamwork, learning how to work with others in reaching the same goals. This club help learners develop social skills and give students an open floor to build confidence and work on their performance anxiety.Because of the comfortable environment, conversations flow easily and everyone is open to new ideas.
-                        Events include competitions such as instrumental,singing dance and many more. Cultural club also celebrates all the festivals creating a vibrant atmosphere all around.
+                        <div className="m-3 font-normal text-xl border-solid border-2 border-orange-600 rounded-xl p-2 text-justify">
+                            Darpan comes you with the opportunity of developing personality in ways through which learn not only to benefit others but ourselves as well. With the aim of developing one's personality we have reached methods like demo interview sessions and group discussions for your placements along with other activities and charities that are to be held.
                         </div>
                     </div>
-                    <div className="text-center font-bold text-3xl font-sans m-3 p-2 border-2 border-solid border-red-600 rounded-xl">
+                    <div className="text-center font-bold text-3xl font-sans m-3 p-2 border-2 border-solid border-orange-600 rounded-xl">
                         Our Events....
                         <div className="flex-col">
                             <div className="flex flex-wrap justify-center m-3 ">
 
-                                <div className="border-solid border-2 m-3 border-red-600 h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[46%]">
+                                <div className="border-solid border-2 m-3 border-orange-600 h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[46%]">
                                     <div flex-col>
                                         <img className="rounded-xl h-56 w-full" src="cultural_logo.png" />
-                                        <div className="text-red-600 border-2 border-solid border-black-200 h-auto text-xl overflow-hidden mt-2">
+                                        <div className="text-orange-600 border-2 border-solid border-black-200 h-auto text-xl overflow-hidden mt-2">
                                             Event name
                                             <div className="text-black mt-2  text-justify font-normal text-sm p-2 ">jsdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir </div>
                                             <div>
-                                                <button className="text-black bg-red-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
+                                                <button className="text-black bg-orange-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div className="border-solid border-2 m-3 border-red-600 h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[46%]">
+                                <div className="border-solid border-2 m-3 border-orange-600 h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[46%]">
                                     <div flex-col>
                                         <img className="rounded-xl h-56 w-full" src="cultural_logo.png" />
-                                        <div className="border-2 border-solid border-black-200 h-auto text-xl overflow-hidden mt-2 text-red-600">
+                                        <div className="border-2 border-solid border-black-200 h-auto text-xl overflow-hidden mt-2 text-orange-600">
                                             Event name
                                             <div className="text-black mt-2 p-2  text-justify font-normal text-sm ">jsdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir </div>
                                             <div>
-                                                <button className="text-black bg-red-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
+                                                <button className="text-black bg-orange-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
 
                             </div>
-                            <div className="font-semibold text-center text-2xl border-2-solid bg-red-400 p-1 ml-8 mr-8 rounded-xl">
+                            <div className="font-semibold text-center text-2xl border-2-solid bg-orange-400 p-1 ml-8 mr-8 rounded-xl">
                                 <button>
                                     Load more Events
                                 </button>
@@ -187,6 +177,7 @@ export default function Culturalclub() {
 
                     </div>
                 </div>
+
             </div>
         </div>
 
