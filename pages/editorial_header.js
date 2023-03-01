379ba1a967bdfe476ec69318a8ea23963344f43f
dev-davinder <davinder@cuecoder.com>
2023-03-01 09:47:03 +0530

    davinder commit

---
+++ pages/editorial_header.js
@@ -0,0 +1,6 @@
+<div className=" h-44 flex justify-center space-x-5 shadow-xl  
+           
+shadow-red-200 items-center">
+    <img className=" h-44" src="editorial_tile2.png" />
+    <img className=" w-[600px] h-20 " src="editorial_tile1.png" />
+</div>
