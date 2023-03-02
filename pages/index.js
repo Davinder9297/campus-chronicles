fd3bbbd84feb4738676344fc690f4927605e8ee5
Jashanjot <kaurjashanjot16@gmail.com>
2023-03-02 11:19:58 +0530

    Jashan 2  commit

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import Carousal from "../components/Carousal"
 import Director2 from "../components/director2"
 import Events from "../components/events"
 import Collab from "../components/collab"
+import Header from "../components/header"
 export default function Index() {
     return(<>
       <div className="flex-col">
-
+<Header/>
 <div className="h-[350px] mt-[2px] "><Carousal/></div>
 <Director2/>
 <Events/>
--- pages/placement.js
+++ pages/placement.js
@@ -1,20 +1,60 @@
 // import Navbar from "../components/navbar";
-export default function Placement(){
-    return (<>
-    <div className="flex justify-between flex-row my-2 border-2">
-      <div className="  flex-col h-80 w-64 bg-slate-300 border-2  border-rose-500">
-    
-        <div className="ml-5 text-2xl bg-black text-gray-800 font-bold">Home</div>
-        <div className="   bg-slate-500">Interview Related questions</div>
-        
-        <div className="   ">Past details about visited companies</div>
-        <div className="   text-center">Take Mock Test</div>
+import { icons } from 'react-icons'
+import { AiTwotoneHome } from 'react-icons/ai'
+import {MdEditNotifications} from 'react-icons/md'
+import {FaThList} from 'react-icons/fa'
 
-      </div> 
-      {/* <div className=" flex border-solid border-4 bg-slate-500 w-60" >
 
-    </div>*/}
-     
+export default function Placement() {
+  return (<>
+  <div className="flex p-2 justify-between flex-row mx-1 my-2 border-2 rounded-md border-purple-900">
+      <div className="  flex-col p-2 text-lg  h-auto w-[22%]  justify-center grid grid-cols-1 divide-y  border-4 rounded-md  border-orange-900">
+
+        <div className=" flex text-gray-800 font-bold  "><div className='mt-4 mb-4'> <button> <AiTwotoneHome /></button> Home </div></div>
+        <div className=" flex text-gray-800 font-bold   border-black"><div className='mt-4 mb-4'>Interview Related questions</div></div>
+
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'>Past details about visited companies</div></div>
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'>Take Mock Test</div></div>
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'>Placement Procedure</div></div>
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'>Take Mock Test</div></div>
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'>Take Mock Test</div></div>
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'>Take Mock Test</div></div>
+
+
+      </div>
+      <div className=" flex border-solid border-4 border-red-600 bg-red-300 w-[50%]" >
+
+      </div>
+
+      <div className=" flex border-solid border-4 w-[22%] border-blue-700" >
+        <div className=' border-2 border-solid border-blue-600 w-[90%] text-center font-bold text-2xl font-sans m-3 p-2'><button> <MdEditNotifications /> </button>
+           Notifications
+          <div className="  flex-col p-2 text-xl  h-auto w-[98%]  justify-center grid grid-cols-1 divide-y  border-y-2 rounded-md  border-orange-900">
+
+        <div className=" flex text-gray-800 font-bold  "><div className='mt-4 mb-4'> <button> <FaThList/></button> 1st not </div></div>
+        <div className=" flex text-gray-800 font-bold   border-black"><div className='mt-4 mb-4'> <button> <FaThList/></button> 1st not</div></div>
+
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'> <button> <FaThList/></button> 1st not</div></div>
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'> <button> <FaThList/></button> 1st not</div></div>
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'> <button> <FaThList/></button> 1st not</div></div>
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'> <button> <FaThList/></button> 1st not</div></div>
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'> <button> <FaThList/></button> 1st not</div></div>
+        <div className=" flex  text-gray-800 font-bold  border-black border-b"><div className='mt-4 mb-4'> <button> <FaThList/></button> 1st not</div></div>
+      </div>
+      </div>
+      </div>
     </div>
-    </>)
+
+    <div className=' w-auto h-80 mx-1 my-2 border-2 border-purple-900 bg-yellow-100 text-2xl'> T/P officer and Stats
+    
+    </div>
+
+    
+    <div className='flex-col mt-8 mb-5 w-auto h-96 mx-1 my-2 border-2 border-purple-900 bg-blue-300 font-bold text-center text-2xl '> Events Organised by T/P Cell
+
+    <div className='flex justify-evenly h-auto m-3 p-2 border-2 border-zinc-700'> </div>
+    </div>
+
+     
+  </>)
 }
