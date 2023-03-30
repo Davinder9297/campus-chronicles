247dbe05ceb1d2f3fd5de502cfb6a48e5a373d6d
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2023-03-30 13:58:43 +0530

    Jas 7 Commit

--- components/header.js
+++ components/header.js
@@ -1,5 +1,6 @@
 import Link from "next/link"
 export default function Header(){
+
     return(<>
     <div className="flex  justify-around pt-2 h-11 font-serif text-md font-semibold bg-purple-300 ">
 <div >Home</div> 
@@ -40,7 +41,29 @@ export default function Header(){
 
 <div><a href="">Placements</a></div>
 
-<div><button>Login</button></div>
+<div><button  type="button" className="bg-black" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Launch
+</button>
+<div className="modal fade " id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog modal-dialog-centered bg-black">
+    <div className="modal-content bg-black">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+        ...
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
 </div>
     </>)
 }
