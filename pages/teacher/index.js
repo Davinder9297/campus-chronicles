6b4dbad57265b759ddf7208f1c9464ad49521f97
dev-davinder <davinder@cuecoder.com>
2023-04-01 19:09:23 +0530

    teacher page created  by davinder

--- pages/teacher/index.js
+++ pages/teacher/index.js
@@ -1,17 +1,17 @@
 export default function Teacher(){
     return(<>
-    <div className="">
-    <div className="text-center font-serif mt-2 text-4xl">Welcome Mrs.Jaspreet Kaur</div>
-    <div className="text-center text-xl mt-4">What Would You Like To Do?</div>
+    <div className="teacherbg h-[84vh] text-white pt-4">
+    <div className="text-center font-serif  text-4xl">Welcome Ms.Jaspreet Kaur</div>
+    <div className="text-center text-xl mt-4 font-s">What Would You Like To Do?</div>
    <div className="flex justify-center mt-4">
     <div className="flex flex-wrap justify-around max-w-5xl w-auto ">
-        <div className="h-32 w-44 bg-slate-400 m-4">df</div>
-        <div className="h-32 w-44 bg-slate-400 m-4">df</div>
-        <div className="h-32 w-44 bg-slate-400 m-4">df</div>
-        <div className="h-32 w-44 bg-slate-400 m-4">df</div>
-        <div className="h-32 w-44 bg-slate-400 m-4">df</div>
-        <div className="h-32 w-44 bg-slate-400 m-4">df</div>
-        <div className="h-32 w-44 bg-slate-400 m-4">df</div>
+        <div className="h-32 w-44 text-center flex justify-center items-center bg-white profile m-4">View Profile</div>
+        <div className="h-32 w-44 bg-slate-400 assignment m-4">Post An Assignment</div>
+        <div className="h-32 w-44 bg-slate-400 m-4">Student Records</div>
+        <div className="h-32 w-44 bg-slate-400 m-4">Attendence</div>
+        <div className="h-32 w-44 bg-slate-400 m-4">Upload Reasearch Papers</div>
+        <div className="h-32 w-44 bg-slate-400 m-4">Announcement Generation</div>
+        <div className="h-32 w-44 bg-slate-400 m-4">FDFA</div>
         <div className="h-32 w-44 bg-slate-400 m-4">df</div>
     </div>
    </div>
