966cc4c71dcc06bd04ffbf755cbf1646026bbdfb
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2023-04-05 15:27:05 +0530

    Jas student pade index made

--- pages/club/load_more_events.js
+++ pages/club/load_more_events.js
@@ -1,45 +1,104 @@
-export default function viewEvent() {
+export default function loadMoreEvent() {
     return (<>
         <div className="">
             <div className="text-center text-4xl h-20 p-3 bg-amber-600 font-semibold font-body">
-                Event Name
+                Club Name
             </div>
 
-            <div className="  h-[75 vh] overflow-hidden">
-                <div className="flex mt-4 ml-4 h-[90%] ">
-                    <div className="w-[40%] p-2">
-                        <img src="/1.jpg" className=" w-full mb-3 h-[48%] rounded-xl" />
-                        <img src="/1.jpg" className=" w-full h-[48%] rounded-xl " />
-                    </div>
+            <div className="flex-col">
+                <div className="flex flex-wrap justify-center m-3 ">
 
-                    <div className=" w-[58%] ml-2 text-center text-4xl mx-auto">
-                        Event Details....
-                        <div className=" overflow-y-scroll h-[59vh] scrollbar-thin scrollbar-track-gray-50 scrollbar-thumb-gray-300 " >
-                            <div className="text-lg w-[90%] text-justify m-auto mt-2 p-2 ">
-                                lore  hjhgrfed rfedwjhge  jytrewyur egtjhrgfedmj efgedw hfu udhcneioj ffcuhifjqwo ruhnfoiafn
-                                Varieties of events are being observed in India on national level in order to increase awareness among
-                                lore  hjhgrfed rfedwjhge  jytrewyur egtjhrgfedmj efgedw hfu udhcneioj ffcuhifjqwo ruhnfoiafn
-                                Varieties of events are being observed in India on national level in order to increase awareness amonglore  hjhgrfed rfedwjhge  jytrewyur egtjhrgfedmj efgedw hfu udhcneioj ffcuhifjqwo ruhnfoiafn
-                                Varieties of events are being observed in India on national level in order to increase awareness amonglore  hjhgrfed rfedwjhge  jytrewyur egtjhrgfedmj efgedw hfu udhcneioj ffcuhifjqwo ruhnfoiafn
-                                Varieties of events are en topic of any event to lore  hjhgrfed rfedwjhge  jytrewyur egtjhrgfedmj efgedw hfu udhcneioj ffcuhifjqwo ruhnfoiafn
-                                Varieties of events are being observed in India on national level in order to increase awareness among
-                                lore  hjhgrfed rfedwjhge  jytrewyur egtjhrgfedmj efgedw hfu udhcneioj ffcuhifjqwo ruhnfoiafn
-                                Varieties of events are being observed in India on national level in order to increase awareness amonglore  hjhgrfed rfedwjhge  jytrewyur egtjhrgfedmj efgedw hfu udhcneioj ffcuhifjqwo ruhnfoiafn
-                                Varieties of events are being observed in India on national level in order to increase awareness amonglore  hjhgrfed rfedwjhge  jytrewyur egtjhrgfedmj efgedw hfu udhcneioj ffcuhifjqwo ruhnfoiafn
-                                Varieties of events are en topic of any event to write some paragraphs or full essay. You can select any of the essay on event which is simple and easy as well as fulfill the need of your children. You are at right place where you can get already prepared essays on any event. You only need to motivate your kids and children to participate in essay writing competition. We have given below varieties of event essay such as Mother’s day essay, Teachers day essay, Republic day essay, Independence day essay and so many.
-                            </div>
-                            <div className="p-2 m-auto flex flex-wrap justify-evenly mt-2">
-                                <img src="/1.jpg" className="h-[22vh] w-[45%] rounded-xl mb-3 " />
-                                <img src="/1.jpg" className="h-[22vh] w-[45%] rounded-xl mb-3 " />
-                                <img src="/1.jpg" className="h-[22vh] w-[45%] rounded-xl mb-3 " />
-                                <img src="/1.jpg" className="h-[22vh] w-[45%] rounded-xl mb-3 " />
+                    <div className="m-3 shadow-yellow-300 shadow-md h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[30%]">
+                        <div flex-col>
+                            <img className="rounded-xl h-56 w-full" src="/cultural_logo.png" />
+                            <div className="text-yellow-600 h-auto text-xl overflow-hidden mt-2">
+                                Event name
+                                <div className="text-black mt-2  text-justify font-normal text-sm p-2 ">jsdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir </div>
+                                <div>
+                                    <button className="text-black bg-yellow-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
+                                </div>
                             </div>
                         </div>
-
-
                     </div>
+
+                    <div className="m-3 shadow-yellow-300 shadow-md h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[30%]">
+                        <div flex-col>
+                            <img className="rounded-xl h-56 w-full" src="/cultural_logo.png" />
+                            <div className="h-auto text-xl overflow-hidden mt-2 text-yellow-600">
+                                Event name
+                                <div className="text-black mt-2 p-2  text-justify font-normal text-sm ">jsdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir </div>
+                                <div>
+                                    <button className="text-black bg-yellow-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="m-3 shadow-yellow-300 shadow-md h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[30%]">
+                        <div flex-col>
+                            <img className="rounded-xl h-56 w-full" src="/cultural_logo.png" />
+                            <div className="text-yellow-600 h-auto text-xl overflow-hidden mt-2">
+                                Event name
+                                <div className="text-black mt-2  text-justify font-normal text-sm p-2 ">jsdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir </div>
+                                <div>
+                                    <button className="text-black bg-yellow-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="m-3 shadow-yellow-300 shadow-md h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[30%]">
+                        <div flex-col>
+                            <img className="rounded-xl h-56 w-full" src="/cultural_logo.png" />
+                            <div className="text-yellow-600 h-auto text-xl overflow-hidden mt-2">
+                                Event name
+                                <div className="text-black mt-2  text-justify font-normal text-sm p-2 ">jsdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir </div>
+                                <div>
+                                    <button className="text-black bg-yellow-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="m-3 shadow-yellow-300 shadow-md h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[30%]">
+                        <div flex-col>
+                            <img className="rounded-xl h-56 w-full" src="/cultural_logo.png" />
+                            <div className="text-yellow-600 h-auto text-xl overflow-hidden mt-2">
+                                Event name
+                                <div className="text-black mt-2  text-justify font-normal text-sm p-2 ">jsdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir </div>
+                                <div>
+                                    <button className="text-black bg-yellow-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="m-3 shadow-yellow-300 shadow-md h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[30%]">
+                                    <div flex-col>
+                                        <img className="rounded-xl h-56 w-full" src="/cultural_logo.png" />
+                                        <div className="text-yellow-600 h-auto text-xl overflow-hidden mt-2">
+                                            Event name
+                                            <div className="text-black mt-2  text-justify font-normal text-sm p-2 ">jsdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir </div>
+                                            <div>
+                                                <button className="text-black bg-yellow-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="m-3 shadow-yellow-300 shadow-md h-auto rounded-xl cursor-pointer hover:scale-105 transition duration-150 w-[30%]">
+                                    <div flex-col>
+                                        <img className="rounded-xl h-56 w-full" src="/cultural_logo.png" />
+                                        <div className="text-yellow-600 h-auto text-xl overflow-hidden mt-2">
+                                            Event name
+                                            <div className="text-black mt-2  text-justify font-normal text-sm p-2 ">jsdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir jkfhvrgniu fbwukjflir jkfhvrgniu sdsnfc fehrbgfliesn shbfliwfc fbwukjflir </div>
+                                            <div>
+                                                <button className="text-black bg-yellow-300 w-full rounded-xl mt-3 p-1 font-semibold">View Event</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                 </div>
             </div>
+
         </div>
     </>)
 }
---
+++ pages/student/my_profile.js
@@ -0,0 +1,38 @@
+export default function Student() {
+    return (<>
+
+        <div className=" w-[90%] m-auto mt-4 text-center text-4xl font-semibold h-[75vh] shadow-md shadow-slate-600 p-4 overflow-hidden">
+            Welcome...  &nbsp;&nbsp; Ms. Jaspreet kaur
+
+            <div className=" w-[93%] m-auto mt-5 text-center text-4xl font-semibold flex justify-between p-4">
+                <div className="w-[30%] shadow-md shadow-slate-500 p-2">
+                    <div >
+                        <img src="/teacher.jpg" className="shadow-md shadow-slate-700 h-64 rounded-full mt-3 m-auto" />
+                    </div>
+                    <div className="mt-14">
+                        Jaspreet kaur
+                    </div>
+                </div>
+                <div className="border-2 border-solid border-green-500 w-[65%] text-2xl">
+                    <div className="h-12 w-[100%] flex "> 
+                        <div className=" w-[50%] m-auto bg-yellow-500 font-semibold p-1 hover:bg-yellow-400 cursor-pointer">
+                            Personal Information
+                        </div>
+                        <div className="bg-amber-800 w-[50%] m-auto p-1  hover:bg-amber-700 cursor-pointer">
+                            jshfiuerhi
+                        </div>
+                    </div>
+
+                    <div className="bg-zinc-500 h-[47vh] overflow-auto scrollbar-thin scrollbar-thumb-zinc-900 text-sm">
+                        jdvndjvn usdhbcsu uhfshf dshcus sbs adhbckuisc ufehcsifchn
+                        jdvndjvn usdhbcsu uhfshf dshcus sbs adhbckuisc ufehcsifchn
+                        jdvndjvn usdhbcsu uhfshf dshcus sbs adhbckuisc ufehcsifchn
+                        jdvndjvn usdhbcsu uhfshf dshcus sbs adhbckuisc ufehcsifchn
+                        jdvndjvn usdhbcsu uhfshf dshcus sbs adhbckuisc ufehcsifchn
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </>)
+}
