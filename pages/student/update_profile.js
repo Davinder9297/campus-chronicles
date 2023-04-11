904cc7dfcaabcc69fa83c9dd93837589396699bd
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2023-04-11 12:08:45 +0530

    Jas student profile page completed

--- pages/student/update_profile.js
+++ pages/student/update_profile.js
@@ -1,58 +1,103 @@
 import Link from "next/link"
 import Navbar from "../../components/navbar"
+import { useState } from "react"
+import { FaEdit } from 'react-icons/fa'
+import { FiSave } from 'react-icons/fi'
+import { CgShapeRhombus } from 'react-icons/cg'
+import { useEffect } from "react"
+import { Chart } from "chart.js";
 
 export default function My_profile() {
-    function View() {
-        html = '';
-    
-    for (let index = 0; index < 10; index++) {
-        html += `
-        <div id="aa">
-        <h4> ${index}</h4>
-        <hr>
-        // <p id="p1">${jas[index]}</p>
-        </div>`;
-    };
-    innerHTML = html;
 
-            <div className="">aa+nfrkjdnfvedfvn dk,</div>
-            console.log("uhfui");
+    const [pinfo, setpinfo] = useState('')
+    const [piclass, setpiclass] = useState('border-b-4 border-black text-black ')
+
+    const [read, setread] = useState(true)
+    const [disable, setdisable] = useState(true)
+    const [phone, setphone] = useState('7009400665')
+    const [email, setemail] = useState('jaspreetkaursaini469@gmail.com')
+    const [linkedin, setlinkedin] = useState('https://www.linkedin.com/in/jaspreet-kaur23/')
+    const [point, setpoint] = useState('opacity-50 cursor-not-allowed')
+
+    const edit=()=>{
+        setread(false)
+        setdisable(false)
+        setpoint('cursor-pointer')
     }
+
+      const save=()=>{
+        setread(true)
+        setdisable(true)
+        setpoint('opacity-50 cursor-not-allowed')
+    }
+
+    const pi = () => {
+        setedqu('hidden');
+        setpinfo(' ');
+        setpiclass('border-b-4 border-black text-black ')
+        setedeqclass('text-white')
+    }
+
     return (<>
-    <div className="h-screen overflow-hiddenm-auto">
-        <Navbar class="shadow-sm"/>
-        {/* <div className=" w-[90%] m-auto text-center text-4xl font-semibold shadow-md shadow-slate-600 p-4 overflow-hidden h-[80%] border-2 border-solid border-blue-500 "> */}
-            {/* Welcome...  &nbsp;&nbsp; Ms. Jaspreet kaur */}
+        <div className="h-screen ">
+            <div className="h-screen">
+                <Navbar class="shadow-sm" />
+                <div className=" w-[90%] m-auto text-center text-4xl font-semibold shadow-md shadow-slate-600 p-4 h-[80%] overflow-hidden ">
+                    Update Your Profile ....
 
-            <div className=" w-[93%] m-auto mt-5 text-center text-4xl font-semibold flex justify-between p-4 h-[70%]  ">
-                <div className="w-[30%] shadow-md shadow-slate-500 p-2 h-[100%] ">
-                    <div className="">
-                        <img src="/teacher.jpg" className="shadow-md shadow-slate-700 h-[60%] rounded-full mt-3 m-auto" />
-                    </div>
-                    <div className="mt-[10%] text-[90%] ">
-                        Jaspreet kaur
-                    </div>
-                </div>
-                <div className="border-2 border-solid border-green-500 w-[60%] text-[4vh] h-[80%] m-auto">
-                    <div className="h-12 w-[100%] flex "> 
-                        <div className=" w-[50%] m-auto bg-yellow-500 font-semibold p-1 hover:bg-yellow-400 cursor-pointer "onClick={View} >
-                            Personal Information
+                    <div className=" w-[97%] m-auto mt-2 text-center text-4xl font-semibold flex justify-between p-4 h-[90%] ">
+                        <div className="flex justify-center  items-center  h-[100%]  w-[30%] shadow-md shadow-slate-400 rounded-sm">
+                            <div className="h-[100%] w-[100%] justify-center">
+                                <div className="h-[80%]">
+                                    <img src="/jass.jpeg" className="shadow-md shadow-slate-700 h-[90%] rounded-full mt-2 m-auto" />
+                                </div>
+                                <div className="text-[80%] ">
+                                    Jaspreet kaur
+                                </div>
+                            </div>
                         </div>
-                        <div className="bg-amber-800 w-[50%] m-auto p-1  hover:bg-amber-700 cursor-pointer">
-                            jshfiuerhi
-                        </div>
-                    </div>
+                        <div className=" w-[65%] shadow-md shadow-slate-400 text-[60%] h-[100%] m-auto">
+                            <div className="h-[]5%] w-[100%] flex ">
+                                <div onClick={pi} className={` w-[100%] m-auto bg-yellow-500 font-semibold p-1 hover:bg-yellow-400 cursor-pointer ${piclass}`} >
+                                    Personal Information
+                                </div>
+                                
+                            </div>
 
-                    <div id="gg" className="bg-zinc-500 h-[90%] overflow-auto scrollbar-thin scrollbar-thumb-zinc-900 text-sm">
-                        jdvndjvn usdhbcsu uhfshf dshcus sbs adhbckuisc ufehcsifchn
-                        jdvndjvn usdhbcsu uhfshf dshcus sbs adhbckuisc ufehcsifchn
-                        jdvndjvn usdhbcsu uhfshf dshcus sbs adhbckuisc ufehcsifchn
-                        jdvndjvn usdhbcsu uhfshf dshcus sbs adhbckuisc ufehcsifchn
-                        jdvndjvn usdhbcsu uhfshf dshcus sbs adhbckuisc ufehcsifchn
+                            <div className={`m-auto  h-[80%] w-[90%] overflow-auto scrollbar-thin pt-3 scrollbar-thumb-zinc-900 text-sm px-2 ${pinfo}`}>
+                                <table className="border-collapse border border-white w-[95%] mx-auto text-lg">
+                                    <tbody>
+                                        
+                                        <tr>
+                                            <td className=" p-2 border-b-2 w-[50%] text-left pl-3 h-auto border-slate-300 ">Contact Number</td>
+                                            <td className=" p-2 border-b-2 w-[50%] h-auto border-slate-300 ">7009400665</td>
+                                        </tr>
+                                        <tr>
+                                            <td className=" p-2 border-b-2 w-[50%] text-left pl-3 h-auto border-slate-300 ">Email i'd</td>
+                                            <td className=" p-2 border-b-2 w-[50%] h-auto border-slate-300 ">jaspreetkaursaini469.gmail.com</td>
+                                        </tr>
+                                        <tr>
+                                            <td className=" p-2 border-b-2 w-[50%] text-left pl-3 h-auto border-slate-300 ">Address</td>
+                                            <td className=" p-2 border-b-2 w-[50%] h-auto border-slate-300 ">Mohali</td>
+                                        </tr>
+                                        <tr>
+                                            <td className=" p-2 border-b-2 w-[50%] text-left pl-3 h-auto border-slate-300 ">Linkedin i'd</td>
+                                            <td className=" p-2 border-b-2 w-[50%] h-auto border-slate-300 ">https://www.linkedin.com/in/jaspreet-kaur23/</td>
+                                        </tr>
+                                        
+                                        </tbody>
+                                </table>
+                                <div className="flex  space-x-5 mt-[5%]m-auto"> 
+                            <button  type="submit" onClick={edit} className="flex items-center space-x-1 bg-blue-800 rounded text-white px-2 py-1 cursor-pointer"><div>Edit</div> <FaEdit  className=""/></button>
+                            <button disabled={disable} onClick={save} className={`flex items-center space-x-1 bg-blue-800 rounded text-white px-2 py-1 ${point}`}><div>Save</div> <FiSave className="" /></button>
+                        </div>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-        {/* </div> */}
     </>)
 }
+// export Example;
