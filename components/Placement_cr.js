0d49af5ad6221136c3342ffa436520c9b0f84a08
Jashanjot <kaurjashanjot16@gmail.com>
2023-04-17 11:59:18 +0530

    This is commit

---
+++ components/Placement_cr.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const PlacementCr = () => {
+  return (
+    <div id="carouselExampleControls" className="order-1 carousel slide w-full flex justify-center" data-bs-ride="carousel">
+        <div className='max-w-[1410px] justify-center flex w-full'>
+    <div className="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div className="carousel-inner  ">
+      <div className="carousel-item active relative float-left w-full " data-bs-interval="1000">
+        <img src="/Qs2.png" className="block w-full h-[86vh] xsm:h-40" alt="..."/>
+      </div>
+      <div className="carousel-item  relative float-left w-full" data-bs-interval="1000">
+        <img src="/Relinnes.png" className="block w-full h-[86vh] xsm:h-40" alt="..."/>
+      </div>
+      {/* <div className="carousel-item relative float-left w-full" data-bs-interval="1500">
+        <img src="2.jpg" className="block w-full h-[350px] xsm:h-40" alt="..."/>
+      </div> */}
+    </div>
+    <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Previous</span>
+    </button>
+    <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span className="carousel-control-next-icon" aria-hidden="true"></span>
+      <span className="visually-hidden">Next</span>
+    </button>
+  </div> 
+  </div> 
+  )
+}
+
+export default PlacementCr
