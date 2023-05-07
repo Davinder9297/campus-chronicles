d8bb75c88cf41ac4c1b68f0323fd0e0c6a46d5be
Jaspreet Kaur <jaspreetkaursaini469@gmail.com>
2023-05-07 11:42:18 +0530

    Jass teacher page created

--- pages/teacher/generate_assignment.js
+++ pages/teacher/generate_assignment.js
@@ -7,95 +7,94 @@ export default function GenerateAssignment() {
     const [selected, setSelected] = useState("");
 
     const changeSelectOptionHandler = (event) => {
-      setSelected(event.target.value);
+        setSelected(event.target.value);
     };
-  
-    /** Different arrays for different dropdowns */
+
     const firstsem = [
-      "Basic Electrical Engineering",
-      "Basic Electrical Engineering (Lab)",
-      "Engineering Drawing",
-      "Mathematics 1",
-      "Semiconductor Physics",
-      "Semiconductor Physics (Lab)"
+        "Mathematics 1",
+        "Engineering Drawing",
+        "Semiconductor Physics",
+        "Semiconductor Physics (Lab)",
+        "Basic Electrical Engineering",
+        "Basic Electrical Engineering (Lab)",
     ];
     const secondsem = [
-      "Chemistry",
-      "Chemistry (Lab)",
-      "English",
-      "English (Lab)",
-      "Mathematics 2",
-      "Workshop for Manufacturing",
-      "Programming for Problem Solving",
-      "Programming for Problem Solving (Lab)"
+        "English",
+        "Chemistry",
+        "English (Lab)",
+        "Mathematics 2",
+        "Chemistry (Lab)",
+        "Workshop for Manufacturing",
+        "Programming for Problem Solving",
+        "Programming for Problem Solving (Lab)"
     ];
-  
+
     const thirdsem = [
-      "DSA",
-      "DSA (Lab)",
-      "OOP",
-      "OOP (Lab)",
-      "Humanities",
-      "IT Workshop",
-      "DE (Lab)",
-      "DE",
-      "Mathematics 3",
+        "DE",
+        "DSA",
+        "OOP",
+        "DE (Lab)",
+        "DSA (Lab)",
+        "OOP (Lab)",
+        "Humanities",
+        "IT Workshop",
+        "Mathematics 3",
     ];
-  
+
     const fourthsem = [
-      "DAA",
-      "DAA (Lab)",
-      "OS",
-      "OS (Lab)",
-      "Mathematics 4",
-      "COA",
-      "COA (Lab)"
+        "OS",
+        "DAA",
+        "COA",
+        "OS (Lab)",
+        "DAA (Lab)",
+        "COA (Lab)",
+        "Mathematics 4",
     ];
-  
+
     const fifthsem = [
-      "DBMS",
-      "DBMS (Lab)",
-      "FLAT",
-      "CN",
-      "CN (Lab)",
-      "ERP",
-      "Programmming in Python",
-      "Programmming in Python (Lab)",
-      "SE",
-      "SE (Lab)"
+        "CN",
+        "SE",
+        "ERP",
+        "FLAT",
+        "DBMS",
+        "SE (Lab)",
+        "CN (Lab)",
+        "DBMS (Lab)",
+        "Programmming in Python",
+        "Programmming in Python (Lab)",
     ];
-  
+
     const sixthsem = [
-      "AI",
-      "AI (Lab)",
-      "ML",
-      "ML (Lab)",
-      "CD",
-      "CD (Lab)",
-      "CC",
-      "CC (Lab)",
-      "HRM",
+        "AI",
+        "ML",
+        "CD",
+        "CC",
+        "HRM",
+        "AI (Lab)",
+        "ML (Lab)",
+        "CD (Lab)",
+        "CC (Lab)",
     ];
 
     const seventhsem = [
-        "AI",
-        "AI (Lab)",
+        "DL",
         "ML",
-        "ML (Lab)",
         "CD",
-        "CD (Lab)",
         "CC",
-        "CC (Lab)",
         "HRM",
-      ];
-    
+        "ML (Lab)",
+        "CD (Lab)",
+        "DL (Lab)",
+        "CC (Lab)",
+    ];
+
     let type = null;
-  
-    let options =<option className="bg-zinc-400 text-center" >-- SELECT SUBJECT --</option >
-    ;
-  
+
+    let options = <option className="bg-zinc-400 text-center" >-- SELECT SUBJECT --</option >
+        ;
+
     if (selected === "First Semester") {
-      type = firstsem;
+        type = firstsem;
     } else if (selected === "Second Semester") {
         type = secondsem;
     } else if (selected === "Third Semester") {
@@ -109,13 +108,10 @@ export default function GenerateAssignment() {
     } else if (selected === "Seventh Semester") {
         type = seventhsem;
     }
-  
-  
-    /** If "Type" is null or undefined then options will be null,
-    * otherwise it will create a options iterable based on our array
-    */
+
+
     if (type) {
-      options = type.map((el) => <option  key={el}>{el}</option>);
+        options = type.map((el) => <option key={el}>{el}</option>);
     }
 
     return (<>
@@ -131,75 +127,53 @@ export default function GenerateAssignment() {
 
                 <div className=" m-auto w-[70%] shadow-md  shadow-zinc-900 h-full">
                     <div>
-                    <select onChange={changeSelectOptionHandler} name="select1" id="select1" className=" outline cursor-pointer outline-1 w-full outline-gray-300 bg-zinc-600 text-white p-2 text-xl rounded-2xl text-center overflow-auto scrollbar-thin scrollbar-thumb-zinc-900">
-                        <option className="bg-zinc-400 " >-- SELECT SEMESTER --</option >
-                        <option className="bg-zinc-400 " >First Semester</option >
-                        <option className="bg-zinc-400 " >Second Semester</option >
-                        <option className="bg-zinc-400 " >Third Semester</option >
-                        <option className="bg-zinc-400 " >Fourth Semester</option >
-                        <option className="bg-zinc-400 " >Fifth Semester</option >
-                        <option className="bg-zinc-400 " >Sixth Semester</option >
-                        <option className="bg-zinc-400 " >Seventh Semester</option >
-                    </select>
+                        <select onChange={changeSelectOptionHandler} name="select1" id="select1" className=" outline cursor-pointer outline-1 w-full outline-gray-300 bg-zinc-600 text-white p-2 text-xl rounded-2xl text-center overflow-auto scrollbar-thin scrollbar-thumb-zinc-900">
+                            <option className="bg-zinc-400 " >-- SELECT SEMESTER --</option >
+                            <option className="bg-zinc-400 " >First Semester</option >
+                            <option className="bg-zinc-400 " >Second Semester</option >
+                            <option className="bg-zinc-400 " >Third Semester</option >
+                            <option className="bg-zinc-400 " >Fourth Semester</option >
+                            <option className="bg-zinc-400 " >Fifth Semester</option >
+                            <option className="bg-zinc-400 " >Sixth Semester</option >
+                            <option className="bg-zinc-400 " >Seventh Semester</option >
+                        </select>
                     </div>
                     <div className="w-full border-2 border-solid border-red-800 ">
-                    <select name="select2" id="select2" className=" outline cursor-pointer outline-1 w-full mx-auto outline-gray-300 bg-zinc-600 text-white p-2 text-xl rounded-2xl text-center border-2 border-solid border-red-800 overflow-auto scrollbar-thin scrollbar-thumb-zinc-900 mt-4 pl-16">
-                        {options}
+                        <select name="select2" id="select2" className=" outline cursor-pointer outline-1 w-full mx-auto outline-gray-300 bg-zinc-600 text-white p-2 text-xl rounded-2xl text-center border-2 border-solid border-red-800 overflow-auto scrollbar-thin scrollbar-thumb-zinc-900 mt-4 pl-16">
+                            {options}
 
-                    </select> 
-</div>
+                        </select>
+                    </div>
 
                     <div className="w-full m-auto">
 
-                    <div className="border-b-2 border-solid border-zinc-300 mt-5 flex">
-                        <div className="text-xl p-2 w-[50%]  text-center">
-                        Title of Assignment
+                        <div className="border-b-2 border-solid border-zinc-300 mt-5 flex">
+                            <div className="text-xl p-2 w-[50%]  text-center">
+                                Title of Assignment
+                            </div>
+                            <div className="w-[50%] my-auto">
+                                <input type="text" className="pl-2 h-8 w-[85%] bg-zinc-200" placeholder="Enter the Title here" ></input>
+                            </div>
+
                         </div>
-                        <div className="w-[50%] my-auto">
-                        <input type="text" className="pl-2 h-8 w-[85%] bg-zinc-200" placeholder="Enter the Title here" ></input>
+                        <div className="border-b-2 border-solid border-zinc-300 mt-3 flex">
+                            <div className="text-xl p-2 w-[50%]  text-center">
+                                Attach File
+                            </div>
+                            <div className="w-[50%] my-auto">
+                                <input type="file" className="h-8 w-[85%]"></input>
+                            </div>
                         </div>
-                            
-                    </div> 
-                     <div className="border-b-2 border-solid border-zinc-300 mt-3 flex">
-                        <div className="text-xl p-2 w-[50%]  text-center">
-                        Attach File
+                        <div className="space-x-16 justify-center flex ">
+                            <button className="shadow-amber-400 shadow-inner rounded bg-amber-800 p-2 mt-9 mb-9 px-5 hover:bg-amber-700 hover:text-white ">Upload</button>
+                            <button className="shadow-amber-400 shadow-inner rounded bg-amber-800 p-2 mt-9 mb-9 hover:bg-amber-700 hover:text-white "><Link href={"/teacher/view_submitted_assignment"}>View Submitted Assignments</Link></button>
+                            <button className="shadow-amber-400 shadow-inner rounded bg-amber-800 p-2 mt-9 mb-9 px-5 hover:bg-amber-700 hover:text-white ">Cancel</button>
                         </div>
-                        <div className="w-[50%] my-auto">
-                        <input type="file" className="h-8 w-[85%]"></input>
-                        </div> 
-                    </div> 
-                    <div className="space-x-16 justify-center flex ">
-                        <button className="shadow-amber-400 shadow-inner rounded bg-amber-800 p-2 mt-9 mb-9 px-5 hover:bg-amber-700 hover:text-white ">Upload</button>
-                        <button className="shadow-amber-400 shadow-inner rounded bg-amber-800 p-2 mt-9 mb-9 hover:bg-amber-700 hover:text-white "><Link href={"/teacher/view_submitted_assignment"}>View Submitted Assignments</Link></button>
-                        <button className="shadow-amber-400 shadow-inner rounded bg-amber-800 p-2 mt-9 mb-9 px-5 hover:bg-amber-700 hover:text-white ">Cancel</button>
-                    </div> 
-                    
-                </div>
+
+                    </div>
                 </div>
             </div>
         </div>
 
-
-        {/* <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
-        <div>
-            <select name="select1" id="select1">
-                <option value="1">Fruit</option>
-                <option value="2">Animal</option>
-                <option value="3">Bird</option>
-                <option value="4">Car</option>
-            </select>
-            <select name="select2" id="select2">
-                <option value="1">Banana</option>
-                <option value="1">Apple</option>
-                <option value="1">Orange</option>
-                <option value="2">Wolf</option>
-                <option value="2">Fox</option>
-                <option value="2">Bear</option>
-                <option value="3">Eagle</option>
-                <option value="3">Hawk</option>
-                <option value="4">BWM</option>
-
-            </select> 
-        </div>*/}
     </>)
 }
