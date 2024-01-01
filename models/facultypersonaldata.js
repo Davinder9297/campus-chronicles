c519a183cbe3a6897d8a8b746342599c597e6166
Davinder9297 <davindergiri765@gmail.com>
2024-01-01 12:27:22 +0530

    commit by davinder

--- models/facultypersonaldata.js
+++ models/facultypersonaldata.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import Email from "next-auth/providers/email";
 mongoose.set("strictQuery", false);
 mongoose.connect("mongodb+srv://campus-chronicles:cc@cc.cvjckys.mongodb.net/test");
 // mongoose.connect("mongodb://localhost:27017/main");
@@ -23,7 +22,7 @@ const ourschema=new mongoose.Schema({
         type:String
     },
     email:{
-        type:Email
+        type:String
     },
     linkedin:{
         type:String
